c479bf53b5ae9c56647b78a161675f6cd54de0ae
Kevin Welch <kaedub16@gmail.com>
2018-11-08 09:32:46 -0800

    Changed Navbar class name to Navbar and passed currentUser from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
+        <Navbar currentUser={testuser} />
         <Routes currentUser={testuser} />
       </div>
     );
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-class App extends Component {
+class Navbar extends Component {
   render() {
     /** NavLink for '/' is given 'exact' to correctly toggle .active */
     return (
@@ -11,10 +11,14 @@ class App extends Component {
         <NavLink className="Navlink" to='/companies'>Companies</NavLink>
         <NavLink className="Navlink" to='/jobs'>Jobs</NavLink>
         <NavLink className="Navlink" to='/profile'>Profile</NavLink>
-        <NavLink className="Navlink" to='/login'>Log In</NavLink>
+        {this.props.currentUser ? 
+          <NavLink className="Navlink" to='/logout'>Log Out</NavLink> :   
+          <NavLink className="Navlink" to='/login'>Log In</NavLink>
+        }
+        
       </nav>
     );
   }
 }
 
-export default App;
+export default Navbar;
