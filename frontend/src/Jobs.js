0673ed586ac53cfc817305a75e394c128de9728f
Kevin Welch <kaedub16@gmail.com>
2018-11-07 20:44:12 -0800

    Jobs shows search bar while loading

--- frontend/src/Jobs.js
+++ frontend/src/Jobs.js
@@ -9,25 +9,34 @@ class Jobs extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { jobs: [] };
+    this.state = { 
+      jobs: [],
+      loading: true,
+      error: null
+    };
   }
 
   /** get all jobs on mount */
   async componentDidMount() {
-    let jobs = await JoblyApi.getJobs();;
-    this.setState({ jobs });
+    const jobs = await JoblyApi.getJobs();;
+    this.setState({ jobs, loading: false });
   }
 
   /** get list of filtered jobs and update state */
   searchJobs = async (search) => {
-    let jobs = await JoblyApi.getJobs(search);
+    const jobs = await JoblyApi.getJobs(search);
     this.setState({ jobs });
   }
 
   render() {
+    if (this.state.error) return <h1>{this.state.error}</h1>
+    if (this.state.loading) return (
+      <div><Search handleSearch={this.searchJobs} />
+      <h3>Loading...</h3></div>
+    );
     return (
       <div className="Jobs">
-        <Search search={this.searchJobs} />
+        <Search handleSearch={this.searchJobs} />
         {this.state.jobs.map(job => <JobCard key={job.id} job={job} />)}
       </div>
     );
--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -2,10 +2,80 @@ import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      email: '',
+      first_name: '',
+      last_name: '',
+      showSignup: false
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleClick(evt) {
+    console.log(`clicked ${evt.target}`);
+  }
+  handleSubmit(evt) {
+    evt.preventDefault();
+  }
+
+  // Control input fields
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value })
+  }
+
   render() {
     return (
-      <div className="Profile">
-        <h1>This will be a Profile form</h1>
+      <div className="Login">
+        <br></br>
+        <button onClick={(evt) => this.handleClick(false)}>Login</button>
+        <button onClick={(evt) => this.handleClick(true)}>Signup</button>
+        <form className="LoginSignupForm" onSubmit={this.handleSubmit}>
+          <br />
+          <label htmlFor="username">Username</label>
+          <input id="username" name="username"
+            value={this.state.username}
+            onChange={this.handleChange}
+            type='text'
+            disabled />
+          <br />
+
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password"
+            value={this.state.password}
+            type='password'
+            onChange={this.handleChange} />
+          <br />
+          {this.state.showSignup ?
+            <React.Fragment>
+              <label htmlFor="first_name">First Name</label>
+              <input id="first_name" name="first_name"
+                value={this.state.first_name}
+                onChange={this.handleChange}
+                type='text'></input>
+              <br></br>
+              <label htmlFor="last_name">Last Name</label>
+              <input id="last_name" name="last_name"
+                value={this.state.last_name}
+                type='text'
+                onChange={this.handleChange} />
+              <br></br>
+              <label htmlFor="email">Email</label>
+              <input id="email" name="email"
+                value={this.state.email}
+                type='email'
+                onChange={this.handleChange} />
+              <br></br>
+            </React.Fragment> :
+            ''
+          }
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
